42f18c677bb493a70109405bd167ed571bd9f96b
ruizhan <yruizhan@163.com>
2024-03-31 21:49:05 +0800

    initial

---
+++ include/process.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "pnc_map.h"
+
+class Process
+{
+public:
+    void planProcess();
+    PNC_Map my_map;
+};
---
+++ src/process.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "process.h"
+using std::cout, std::endl;
+void Process ::planProcess()
+{
+    cout << "this is planning process" << endl;
+    my_map.mapInfo();
+    cout << "planning sucess!" << endl;
+}
